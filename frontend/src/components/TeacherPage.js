803263c122775332982742223a59eaaa49e52221
mahrezkhan <mahrezkhan18@gmail.com>
2025-06-23 09:44:49 +0600

    worked in 2nd page

--- frontend/src/components/TeacherPage.js
+++ frontend/src/components/TeacherPage.js
@@ -1,5 +1,16 @@
+import { useState } from "react";
 const TeacherPage = () => {
-  return <h1>Welcome to the Teacher Portal</h1>;
+  const [name,changedname]=useState('mahrez');
+  const HandleClick=(name)=>{
+    changedname('mahzabin');
+  }
+  return (
+    <div >
+      <h1>Welcome to the Teacher Portal</h1>
+      <p>{name}</p>
+      <button onClick={()=>HandleClick(name)}>click</button>
+    </div>
+  );
 };
 
 export default TeacherPage;
